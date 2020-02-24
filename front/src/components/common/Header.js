e31009e83dd4473373acee2bd3e55f28505bbd10
macmagic <arroyes89@gmail.com>
2020-02-24 22:25:35 +0100

    Add Header

--- front/src/components/common/Header.js
+++ front/src/components/common/Header.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    }
+}));
 
 const Header = (props) => {
+    const classes = useStyles();
     return (
         <header>
-            <nav className="nav-header">
-                <h2>Hola!</h2>
-                <img className="header-image" width="50px" height="50px" src="golang.png"></img>
-            </nav>
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h3" className={classes.title}>
+                            API REST with GO
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
         </header>
     )
 }
